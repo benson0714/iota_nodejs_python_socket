bb08ebae500fb42bdb63621f6dc7c5fb69de35f1
Benson <benson0714@gmail.com>
2022-12-14 15:53:14 +0800

    add mongoDB upload

--- python/dht11_mongoDB_socket.py
+++ python/dht11_mongoDB_socket.py
@@ -2,6 +2,8 @@ import socket
 import Adafruit_DHT
 import time
 import json
+import pymongo
+import bson.json_util as json_util
 
 # dht11 object and raspi pin
 DHT_SENSOR = Adafruit_DHT.DHT11
@@ -22,9 +24,13 @@ def dht11_collect():
             print(now_time)
             print("Temp={0:0.1f}C Humidity={1:0.1f}".format(temperature, humidity))
             data_dict = {"Time":now_time, "Temp":temperature, 'Humidity': humidity}
+
+            # upload to mongoDB
+            mongoDB_upload(json.loads(json_util.dumps(data_dict)))
+
+            # change dict to string
             data_str = json.dumps(data_dict)
             print(data_str)
-
             # send data to nodejs for upload to iota
             socket_client(data_str)
 
@@ -40,6 +46,14 @@ def socket_client(data):
     print('recv: ' + indata.decode())
 
     s.close()
+
+def mongoDB_upload(data):
+    myclient = pymongo.MongoClient("mongodb://TestUser:password@140.120.40.132:27017/")
+    mydb = myclient["test"]
+    dblst = myclient.list_database_names()
+    mycol = mydb["dht11_mongoDB_test"]
+    collst = mydb.list_collection_names()
+    x = mycol.insert_one(data)
 
 if __name__ == '__main__':
     dht11_collect()
